ecbb95140a0721d527db7abe607d3cdb408987bc
Sarah Le Cornec <sarahlecornec123@gmail.com>
2016-02-13 09:09:30 +0000

    Initial push

--- app/controllers/todo_list_controller.rb
+++ app/controllers/todo_list_controller.rb
@@ -1,2 +1,6 @@
 class TodoListController < ApplicationController
+
+	def new
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+	root "todo_list#home"
+	get "new" => "todo_list#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
